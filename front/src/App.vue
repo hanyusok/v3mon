d39bb6e184ff4d07f4e854e110272b002e39756d
hanyusok <hanyusok@gmail.com>
2023-11-03 01:53:41 +0000

    2nd edit

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,16 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  
 
   <RouterView />
 </template>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
+import axios from 'axios'
+
+
 
 </script>
 
 <template>
   <main>
-  
+    <h1>this is home</h1>
   </main>
 </template>
